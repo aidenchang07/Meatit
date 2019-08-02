bf0a9cb165b62ee55594ec2391118d95e5418eaa
robinaiden <a10423041@yuntech.org.tw>
2019-08-02 18:17:07 +0800

    1.2019/08/02 18:16 update

---
+++ app/src/main/java/com/example/meatit/introAdapter.java
@@ -0,0 +1,24 @@
+package com.example.meatit;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.viewpager.widget.PagerAdapter;
+
+public class introAdapter extends PagerAdapter {
+
+    Context context;
+    LayoutInflater layoutInflater;
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return false;
+    }
+}
