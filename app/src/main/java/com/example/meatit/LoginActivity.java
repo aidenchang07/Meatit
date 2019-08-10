36e82d5c5e3be7c146dd7718d5f0aa5e5007c618
robinaiden <a10423041@yuntech.org.tw>
2019-08-10 11:45:32 +0800

    update 2019/08/10 11:47 a.m 1.新增logo

--- app/src/main/java/com/example/meatit/LoginActivity.java
+++ app/src/main/java/com/example/meatit/LoginActivity.java
@@ -2,7 +2,10 @@ package com.example.meatit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        //直接返回桌面，不直接關閉應用程式
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            Intent home = new Intent(Intent.ACTION_MAIN);
+            home.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            home.addCategory(Intent.CATEGORY_HOME);
+            startActivity(home);
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
