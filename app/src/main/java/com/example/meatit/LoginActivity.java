7916d864da2f25b44182fd3b3ccc569fd5ba7152
robinaiden <a10423041@yuntech.org.tw>
2019-08-19 10:55:45 +0800

    update 2019/08/19
    1.add google login logic
    2.modify .gitignore

--- app/src/main/java/com/example/meatit/LoginActivity.java
+++ app/src/main/java/com/example/meatit/LoginActivity.java
@@ -1,28 +1,86 @@
 package com.example.meatit;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "LoginActivity";
+    private static final int RC_SIGN_IN = 9001;
 
     private int viewID;
     private Button createAccountButton;
     private Button loginButton;
+    private Button btnCreateAccount;
+    private Button btnLoginFacebook;
+    private Button btnLoginGoogle;
+    // [START declare_auth]
+    private FirebaseAuth mAuth;
+    // [END declare_auth]
+    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        createAccountButton = findViewById(R.id.button8);
-        loginButton = findViewById(R.id.button7);
-        createAccountButton.setOnClickListener(this);
-        loginButton.setOnClickListener(this);
+        findViewById(R.id.btn_login_origin).setOnClickListener(this);
+        findViewById(R.id.btn_create_account).setOnClickListener(this);
+        findViewById(R.id.btn_login_fb).setOnClickListener(this);
+        findViewById(R.id.btn_login_google).setOnClickListener(this);
+
+        // [START config_signin]
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build();
+        // [END config_signin]
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        // [START initialize_auth]
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        // [END initialize_auth]
+
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        if (currentUser != null) {
+            Toast.makeText(this, "Google帳號已登入", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "Google帳號未登入", Toast.LENGTH_SHORT).show();
+        }
 
     }
 
@@ -39,18 +97,73 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     }
 
     @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
+                // [START_EXCLUDE]
+                Toast.makeText(this, "Google帳號登入失敗", Toast.LENGTH_SHORT).show();
+                // [END_EXCLUDE]
+            }
+        }
+
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        Log.d(TAG, "firebaseAuthWithGoogle:" + acct.getId());
+
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(LoginActivity.this, "signInWithCredential:success", Toast.LENGTH_SHORT).show();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(LoginActivity.this, "Authentication Failed.", Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+    }
+
+    @Override
     public void onClick(View view) {
         viewID = view.getId();
-        if (viewID == R.id.button7) {
+        if (viewID == R.id.btn_login_origin) {
             //login
             Intent searchActivityIntent = new Intent(LoginActivity.this, SearchActivity.class);
             startActivity(searchActivityIntent);
-        } else if (viewID == R.id.button8) {
+        } else if (viewID == R.id.btn_create_account) {
             //create account
             Intent createAccountActivityIntent = new Intent(LoginActivity.this, CreateAccountActivity.class);
             startActivity(createAccountActivityIntent);
-        } else {
-
+        } else if (viewID == R.id.btn_login_fb) {
+            Toast.makeText(this, "使用Facebook登入", Toast.LENGTH_SHORT).show();
+        } else if (viewID == R.id.btn_login_google) {
+            signIn();
+            Toast.makeText(this, "使用Google登入", Toast.LENGTH_SHORT).show();
         }
     }
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
 }
+
